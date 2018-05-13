2580676676683ba828b56c3e9c0e5642d619f7de
Clifford Pereira <clifford@jubi.ai>
2018-05-13 22:16:19 +0530

    added main file

--- src/main.go
+++ src/main.go
@@ -7,7 +7,7 @@ import ("fmt"
        )
 
 func main(){
-    http.HandleFunc("/createList",createList.createList());
+    http.HandleFunc("/createList",createList);
     http.HandleFunc("/deleteList",deleteList);
     http.HandleFunc("/addTask",addTask);
     http.HandleFunc("/deleteTask",deleteTask);
@@ -17,6 +17,44 @@ func main(){
 
 var collection=map[string]interface{}{
 };
+
+func createList(w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+    var data=map[string]interface{}{
+        
+    };
+	err := decoder.Decode(&data)
+    if err != nil {
+		panic(err)
+	}
+    var output=map[string]interface{}{
+    };
+    
+    if val, ok := data["Name"]; ok {
+        fmt.Println(val);
+        fmt.Println(ok);
+        fmt.Println(data["Name"])
+        key :=data["Name"].(string);
+        fmt.Println(key);
+        if obj,exists := collection[key]; !exists {
+            fmt.Println(obj);
+            collection[string(key)]=map[string]interface{}{
+            }
+            output=collection;
+        } else {
+            output["data"]="List Existing";
+        }
+    } else {
+        output["data"]="Please specify name of the list";
+    }
+    final , err := json.Marshal(&output)
+    if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+    w.Header().Set("content-type", "application/json")
+	w.Write(final)
+}
 
 func deleteList(w http.ResponseWriter, req *http.Request) {
 	decoder := json.NewDecoder(req.Body)
